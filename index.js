ed21aa01c722c011587e1e86001cb28865aff594
travischantravis <chancheukyin4463@gmail.com>
2019-07-15 12:53:03 +0800

    port from environment variable

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ require('dotenv').config();
 
 
 const app = express();
-app.listen(3000, () => console.log('listening at 3000'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Starting server at ${port}`));
 app.use(express.static('public'));
 app.use(express.json({
     limit: '1mb'
